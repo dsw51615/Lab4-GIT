92f5330e8028b071f5a3b160d16996b6faece865
Wojtek <dsw51615@student.dsw.edu.pl>
2024-06-30 14:06:53 +0200

    Dodanie obslugi flagi

--- skrypt.sh
+++ skrypt.sh
@@ -2,14 +2,14 @@
 
 show_help(){
     echo "Dostepne opcje:"
-    echo "--date	-d Pokaz dzisiejsza date"
-    echo "--logs [N]	-l Stworz plik N log (domyslnie: 100)"
-    echo "--help	-h Pokaz pomoc"
+    echo "--date -d Pokaz dzisiejsza date"
+    echo "--logs -l [N] Stworz plik N log (domyslnie: 100)"
+    echo "--help -h Pokaz pomoc"
 }
 
 create_logs(){
     num_files=${1:-100}
-    for i in $(deq 1 $num_files); do
+    for i in $(seq 1 $num_files); do
 	echo -e "Nazwa pliku: log$i.txt\nScript name: $0\nDate: $(date)" > log$i.txt
     done
 }
