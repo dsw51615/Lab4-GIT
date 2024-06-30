21fed5cf01efb47bb86c079990dd317026f8b3a5
Wojtek <dsw51615@student.dsw.edu.pl>
2024-06-30 13:43:35 +0200

    Add --logs opcja do tworzenia plikow log

--- skrypt.sh
+++ skrypt.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
+show_help(){
+    echo "Dostepne opcje:"
+    echo "--date	-d Pokaz dzisiejsza date"
+    echo "--logs [N]	-l Utworz plik N log (domyslnie 100)"
+    echo "--help	-h Pokaz pomoc"
+}
+
+create_logs(){
+    num_files=${1:-100}
+    for i in$(seq 1 $num_files); do
+	echo -e "Nazwa pliku: log$i.txt\nScript name: $0\nDate: $(date)" > log$i.txt
+    done
+}
+
 case "$1" in
     --date|-d)
 	date
 	;;
+    --logs|-l)
+	create_logs "$2"
+	;;
     --help|-h)
-	echo "Dostepne opcje:"
-	echo "--date -d  Pokaz dzisiejsza date"
-	echo "--help -h  Pokaz pomoc"
+	show_help
 	;;
     *)
-	echo "Opcja niedostepna. Uzyj --help lub --h w celu uzyskania pomocy"
+	echo "Niedostepna opcja. Uzyj --help lub -h w celu uzyskania pomocy."
 	;;
-
 esac
